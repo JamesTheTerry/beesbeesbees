2736c71a61040c5d19a1d240a38089451730c760
James Terry <jamesnorthterry@gmail.com>
2017-10-16 12:22:25 -0700

    Implements everything Grub, Bee, HoneyMakerBee, ForagerBee, and RetiredForagerBee have all been implement. Grub being the ultimate super class. And all the tests pass.

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,14 @@
 var Grub = function() {
+  // age property
+  // color property
+  // food property
+  // eat method
+
+  this.age = 0;
+  this.color = 'pink';
+  this.food = 'jelly';
 };
 
+Grub.prototype.eat = function () {
+  console.log('Chowing down on that sweet sweet ' + this.food);
+};
